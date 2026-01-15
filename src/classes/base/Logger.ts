9cc083c31f009788ddbc5dcddf41d61cc8c82986
mustafakyia <fhyroxx@gmail.com>
2026-01-15 17:54:11 +0300

    log

--- src/classes/base/Logger.ts
+++ src/classes/base/Logger.ts
@@ -1,7 +1,26 @@
+import chalk from "chalk";
+
 export class LoggerInstance {
-	constructor() {}
-	log(...args) {}
-	warn(...args) {}
-	error(...args) {}
-	debug(...args) {}
+	constructor() { }
+
+	private get timestamp(): string {
+		return chalk.gray(`[${new Date().toLocaleTimeString()}]`);
+	}
+
+	log(...args: any[]) {
+		console.log(this.timestamp, chalk.bgBlue.bold("LOG"), ...args);
+	}
+
+	warn(...args: any[]) {
+		console.warn(this.timestamp, chalk.bgYellow.bold("WARN"), ...args);
+	}
+
+	error(...args: any[]) {
+		console.error(this.timestamp, chalk.bgRed.bold("ERROR"), ...args);
+	}
+
+	debug(...args: any[]) {
+		console.debug(this.timestamp, chalk.bgMagenta.bold("DEBUG"), ...args);
+	}
 }
+
